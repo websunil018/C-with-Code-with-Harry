3cacf99622c8658d57ff93739adf3e28e421f037
websunil018 <160797820+websunil018@users.noreply.github.com>
2024-03-28 09:54:10 +0530

    Add files via upload

---
+++ Tutorial64.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include <stdlib.h>
+int main()
+{
+    FILE *ptr = NULL;
+    char string[64] = "This content was produced by Tutorial64.c";
+
+    // ****** Reading a file ******
+    // ptr = fopen("myfile.txt", "r");
+    // fscanf(ptr, "%s", string);
+    // printf("The content of this file has %s\n", string);
+
+    // ****** Witing a file ******
+    ptr = fopen("myfile.txt", "a");
+    fprintf(ptr, "%s", string);
+    return 0;
+}
---
+++ Tutrioal66.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    // You have to fill in values to a template letter.txt
+    // Letter.txt looks something like this:
+    // Thanks {{name}} for purchasing {{item}} from our outlet {{outlet}}.
+    // Please visit our outlet {{outlet}} for any kind of problems. We plan to server you again soon.
+    // You have to read this file and replace these values:
+    // {{name}}   - Harry 
+    // {{item}}   - Table Fan 
+    // {{outlet}} - Ram Laxmi fan outlet
+
+    // Use file functions in c to accomplish the same
+
+    return 0;
+}
